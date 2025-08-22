cd0610050cc0e78a726c1c1bcfe944cf9a887080
jayson <168834011+Jay7963@users.noreply.github.com>
2025-08-22 11:08:53 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,21 +1,8 @@
-CREATE DATABASE pay_db;
-use pay_db;
 
-CREATE TABLE PAYMENTS(
-  CheckNumber varchar(50) PRIMARY KEY,
-  PaymentDate DATE NOT NULL,
-  amount decimal (10,2) NOT NULL
-  );
   
 SELECT CheckNumber,PaymentDate,amount
 FROM PAYMENTS;
 
-
-CREATE TABLE orders(
-  orderDate DATE PRIMARY KEY,
-  requiredDate DATE NOT NULL,
-  status enum("In process") NOT NULL
-  );
   
 SELECT orderDate,requiredDate,status
 FROM orders
@@ -23,36 +10,13 @@ where status = "In process"
 order by orderDate desc;
 
 
-CREATE TABLE employees(
-  employeeNumber INT  PRIMARY KEY,
-  firstName varchar(50) not null,
-  LastName varchar(50) not null,
-  email varchar(100) not null,
-  jobTitle enum("Sales Rep") not null
-  );
-  
  SELECT  firstName,LastName,email
  FROM employees
  where jobTitle = "Sales Rep"
  order by employeeNumber DESC;
 
-CREATE TABLE offices(
-Jina varchar(50) PRIMARY KEY
-);
 
 SELECT *
 FROM offices;
-
-
-CREATE TABLE Products(
-   productName varchar(50) PRIMARY KEY,
-   quantityInStock  FLOAT NOT NULL,
-   buyPrice float NOT NULL
-   );
-   
-SELECT productName,quantityInStock
-FROM Products
-order by buyPrice asc
-LIMIT 5;
 
     
